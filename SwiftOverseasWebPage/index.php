a3f0f69a2917104a2f248187af8975e6605cd613
Samim Hossen <85032804+samim5274@users.noreply.github.com>
2024-11-04 15:43:28 +0600

    Swift Overseas

---
+++ SwiftOverseasWebPage/index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Swift &#8211; Overseas Tours & Travels</title>
+    <link rel="icon" type="image/x-icon" href="img/logo.png">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+
+<?php
+
+include "menu.php";
+
+?>
+
+
+<section id="slide-section" class="pb-4 mb-4">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/slide1.jpg" class="d-block w-100" alt="Image not found...!">
+                <div class="carousel-caption d-none d-md-block">
+                    <h1>Largest Man Power</h5>
+                    <h3>Travel Agency of Bangladesh</h3>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/slide2.jpg" class="d-block w-100" alt="Image not found...!">
+                <div class="carousel-caption d-none d-md-block">
+                <h1>Largest Man Power</h5>
+                <h3>Travel Agency of Bangladesh</h3>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/slide3.jpg" class="d-block w-100" alt="Image not found...!">
+                <div class="carousel-caption d-none d-md-block">
+                <h1>Largest Man Power</h5>
+                <h3>Travel Agency of Bangladesh</h3>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</section>
+
+
+
+<div id="welcome-section"  class="pb-4 mb-4">
+    <div class="container">
+        <div class="row py-4 text">
+            <div class="col-md-6 mt-4">
+                <h3>Welcome to Swift Overseas</h3>
+                <p class="justify-text lead">
+                We are proud to introduce you to Swift Overseas, as one of the leading government- recognized recruitment agencies and labor consultants in Bangladesh, providing unrivaled recruitment and workforce management services, and one of the few organizations that have fulfilled all the conditions from a reliable labor recruitment agency. Bangladesh is a densely populated country and a well-developed education and training system makes them active and qualified to meet the demands of the domestic and foreign markets. We are driven by integrity, commitment, hard work, rewards, and the drive to excel, to deliver excellent client-focused recruitment service coupled with a smart, consultative and honest approach to your clients and candidates. During the last decade, Bangladesh exported a total of around one million different types of labor to Middle Eastern countries. 
+                </p>
+            </div>
+            <div class="col-md-6 mt-4 p-4">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/2Lxvx6uj31M?si=igxKW9L5iCD4j9T-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- user service section -->
+
+
+<div id="our-service-section" class="pb-4 mb-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 m-4">
+                <h2 class="title">Our Services</h2>
+                <P class="justify-text lead">
+                We are the leading consultants in terms of staffing and hiring of labor. we are dedicated to supplying both skilled and unskilled labor depending on the needs of our customers. After we hire candidates, the responsibilities of approaching, hiring, training, and even rewarding them rest with us.
+                </P>
+            </div>
+        </div> 
+        <div class="row">
+            slide
+        </div>       
+    </div>
+</div>
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+<script src="js/main.js"></script>
+    
+</body>
+</html>
