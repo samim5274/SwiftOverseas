10678eabd84f9750157a6143580e852087c6e496
Samim Hossen <85032804+samim5274@users.noreply.github.com>
2024-11-06 10:04:58 +0600

    Swift Overseas

---
+++ Swift/js/main.js
@@ -0,0 +1,65 @@
+// 1st slide section JS  start
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+// 1st slide section JS  end
+
+// flag slide section start
+
+let slideIndexx = 1;
+showSlides(slideIndexx);
+
+function plusSlides(n) {
+  showSlides(slideIndexx += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndexx = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndexx = 1}
+  if (n < 1) {slideIndexx = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexx-1].style.display = "block";
+  dots[slideIndexx-1].className += " active";
+  captionText.innerHTML = dots[slideIndexx-1].alt;
+}
+
+// flag slide section end
---
+++ Swift/slide/main.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.slider-wrapper', {
+
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0: {
+            slidesPreView: 1
+        },
+        768: {
+            slidesPreView: 2
+        },
+        1024: {
+            slidesPreView: 3
+        },
+    }
+  });
